e6114cbba3dd5277255f04cf409dcc9373d46c47
gokulkrishnakr <gokulkrishnache6@gmail.com>
2026-04-22 13:46:45 +0530

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,12 @@
+a=10
+b=20
+c=a+b
+print("The sum of a and b is ",c)
+d=a*b
+print("The product of a and b is ",d)
+e=a/b
+print("The division of a and b is ".e)
+f=a-b
+print("The difference of a and b is",f)
+h=a%b
+print("The modulus of a and b is",h)
